3f972be4b2bf7f27136e5f13fc3a2e170bc70878
Richard Budek <richardbudek@Richards-Air.attlocal.net>
2018-01-29 05:49:04 -0600

    Rev. 1
    First page works, work on other pages

---
+++ Bootstrap-Portfolio/assets/script.js
@@ -0,0 +1,21 @@
+
+/* script.js  by Rich Budek 01/26/2018  */
+/*            for Portfolio-Bootstrap   */
+
+/* routine to make a common nav bar across all of the pages */
+
+document.getElementById("navBarCommon").innerHTML = 
+'<div class="container" id="navBarContainer">' + 
+'<div class="navbar-header">' + 
+'    <a class="navbar-brand" href="#">Rich Budek</a>' +
+'</div>' + 
+'<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' +
+'    <ul class="nav navbar-nav navbar-right">' +
+'        <li><a href="index.html">About</a></li>' +
+'        <li><a href="portfolio.html">Portfolio</a></li>' +
+'        <li><a href="contact.html">Contact</a></li>' +
+'    </ul>' + 
+'</div>' + 
+'</div>';
+
+
